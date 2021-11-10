152d295abde5ccb042121513b2870630c385a6ff
Theodore Basmanov <chatta54@gmail.com>
2021-11-10 12:21:34 +0300

    Timing
    Added page load time to the footer.

--- JS/Timing.js
+++ JS/Timing.js
@@ -1,6 +1,7 @@
 (function(){
     startTime = (new Date).getTime();
     window.addEventListener('load',function () {
+        document.getElementById("timing").innerHTML = "Page load time is " + (((new Date).getTime() - startTime) / 1000) + " Seconds";
         console.log("Page load time is " + (((new Date).getTime() - startTime) / 1000) + " Seconds");   
     });
 })();
